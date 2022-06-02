4f345a319157a1b68a25ff1c4d0046074c178f5d
Alexis BOULIN <aboulin@txmath65.unice.fr>
2022-06-02 13:51:50 +0200

    model_1CV_2400

--- model_1CV_par.py
+++ model_1CV_par.py
@@ -228,7 +228,7 @@ d2 = 800
 d = d1 + d2
 n_sample = 2400
 n_iter = 100
-_alpha_ = np.array([0.25,0.5,0.75,1.0,1.25,1.5,1.75,2.0,2.25,2.5,2.75,3.0,3.25,3.5,3.75,4.0,4.25,4.5,4.75,5.0]) * np.sqrt(np.log(d) / n_sample )
+_alpha_ = np.array([0.25,0.5,0.75,1.0,1.25,1.5,1.75,2.0,2.25,2.5,2.75,3.0,3.25,3.5,3.75,4.0,4.25,4.5,4.75,5.0]) * np.sqrt(np.log(d) / 800 )
 pool = mp.Pool(processes= 10, initializer=init_pool_processes)
 mode = "ECO"
 stockage = []
